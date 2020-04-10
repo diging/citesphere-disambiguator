0b2b1ea8b9d30b3c6ad3cccbea49a9551b330e08
Julia Damerow <julia.damerow@gmail.com>
2020-04-10 17:22:48 -0400

    initial commit

---
+++ citesphere-disambiguator/src/main/java/edu/asu/diging/citesphere/disambiguator/config/MvcConfig.java
@@ -0,0 +1,9 @@
+package edu.asu.diging.citesphere.disambiguator.config;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("edu.asu.diging.simpleusers.web")
+public class MvcConfig {
+
+}
